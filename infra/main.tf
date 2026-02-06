fb3ef1ff5f936ee9dbbd9ca9aacad0c2cddecb73
Paolahz1 <101817321+Paolahz1@users.noreply.github.com>
2026-02-05 21:01:02 -0500

    prueba para crear un recurso en AWS

--- infra/main.tf
+++ infra/main.tf
@@ -1,12 +1,19 @@
-locals {
-  name = "${var.project}-${var.env}"
+resource "aws_s3_bucket" "this" {
+  bucket = "my-unique-bucket-name-12345"
 }
 
-resource "random_id" "suffix" {
-  byte_length = 4
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.this.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
 
-resource "aws_s3_bucket" "test" {
-  bucket        = "${local.name}-tf-test-${random_id.suffix.hex}"
-  force_destroy = true
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
